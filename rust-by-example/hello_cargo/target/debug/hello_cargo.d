f9d24c5102b3b8dd69f074087071a2b061165be1
pathbox <cary.luo@ucloud.cn>
2019-08-30 15:51:19 +0800

    hello cargo:cargo基本使用

---
+++ rust-by-example/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/pathbox/code/learning-rust/rust-by-example/hello_cargo/target/debug/hello_cargo.dSYM: /Users/pathbox/code/learning-rust/rust-by-example/hello_cargo/src/main.rs
---
+++ rust-by-example/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/pathbox/code/learning-rust/rust-by-example/hello_cargo/target/release/hello_cargo.dSYM: /Users/pathbox/code/learning-rust/rust-by-example/hello_cargo/src/main.rs
