4314b297427cab84310ece2f67b066abc53cdc7c
pathbox <guijihe@163.com>
2019-09-17 09:04:13 +0800

    rust char用单引号,字符串用双引号

--- rust-by-example/common/src/main.rs
+++ rust-by-example/common/src/main.rs
@@ -10,4 +10,14 @@ fn main() {
     let _product = 4 * 10;
     let _quotient = 56.7 /11.1;
     let _remainder = 43 % 2;  // rust 常亮建议用_开头
+
+    let _t = true;
+    let _f: bool = false; // 显示指定类型注解
+
+    let _c = 'z';
+    let _heart_eyed_cat = '😻';
+    let _c = 'a'; // 重新初始赋值
+    let _ss = "abcd";
+    println!("The value of _ss is: {}", _ss);
+    println!("The value of cat is: {}", _heart_eyed_cat);
 }
---
+++ rust-by-example/common/target/debug/common.d
@@ -0,0 +1,1 @@
+/mnt/e/CaryCode/learning-rust/rust-by-example/common/target/debug/common: /mnt/e/CaryCode/learning-rust/rust-by-example/common/src/main.rs
