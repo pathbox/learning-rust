b835734d6322e9cb7853ff9430144a5f350f88dd
pathbox <guijihe@163.com>
2019-10-05 16:05:11 +0800

    trait for println! struct

--- rust-by-example/struct_example/src/main.rs
+++ rust-by-example/struct_example/src/main.rs
@@ -14,6 +14,24 @@ fn main() {
     let origin = Point(0, 0, 0);
 
     println!("email: {}", user.email);
+
+    let rect1 = (30,50);
+
+     println!(
+        "The area of the rectangle is {} square pixels.",
+        area(rect1)
+    );
+
+    let rect2 = Rectangle {width: 30, height: 50}; 
+     println!(
+        "The area2 of the rectangle is {} square pixels.",
+        area2(&rect2)
+    );
+
+    let rect3 = Rectangle { width: 30, height: 50 };
+
+    println!("rect3 is {:?}", rect3);
+     println!("rect3 is {:#?}", rect3);
 }
 
 struct User {
@@ -31,11 +49,18 @@ fn build_user(email: String, username: String) -> User {
         active: true, 
         sign_in_count: 1,
     }
+}
 
-    // User {
-    //     email,
-    //     username,
-    //     active: true,
-    //     sign_in_count: 1,
-    // }
+fn area(dimensions: (u32, u32)) ->u32 {
+    dimensions.0 * dimensions.1 
+}
+
+#[derive(Debug)] //增加注解来派生 Debug trait，并使用调试格式打印 Rectangle 实例
+struct Rectangle {
+    width: u32, 
+    height: u32, 
+}
+
+fn area2(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
