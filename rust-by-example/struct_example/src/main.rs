3cb82598d860562c176210c98d3816e3fa905d26
pathbox <guijihe@163.com>
2019-10-05 17:36:19 +0800

    通配符匹配

--- rust-by-example/struct_example/src/main.rs
+++ rust-by-example/struct_example/src/main.rs
@@ -51,6 +51,15 @@ fn main() {
     let result1 = value_in_cents(p);
     println!("The Penny Value is {}", result1);
 
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+
 }
 
 struct User {
@@ -150,3 +159,14 @@ enum UsState {
 //     Dime,
 //     Quarter(UsState),
 // }
+
+// fn plus_one(x: Option<i32>) -> Option<i32> {
+//     match x {
+//         None => None,
+//         Some(i) => Some(i + 1),
+//     }
+// }
+
+// let five = Some(5);
+// let six = plus_one(five);
+// let none = plus_one(None);
