8566990e95f617f897c0cdd541b551342afa3f0a
pathbox <guijihe@163.com>
2019-10-06 23:11:56 +0800

    HashMap example

--- rust-by-example/common_set/src/main.rs
+++ rust-by-example/common_set/src/main.rs
@@ -4,6 +4,7 @@ enum SpreadsheetCell {
     Text(String),
 }
 
+use std::collections::HashMap;
 
 fn main() {
     println!("Common Set");
@@ -65,4 +66,39 @@ fn main() {
 
     let s10 = format!("{}-{}-{}", s7, s8, s9); // 格式化字符串输出
     println!("s10 is {}", s10);
+
+    // HashMap
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let teams  = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores1: HashMap<_,_> = teams.iter().zip(initial_scores.iter()).collect();
+
+    let mut scores2 = HashMap::new();
+
+    scores2.insert(String::from("Blue"), 10);
+    scores2.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score2 = scores2.get(&team_name);
+
+    for (key, value) in &scores2 {
+        println!("{}: {}", key, value);
+    }
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
