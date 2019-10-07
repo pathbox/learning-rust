b7723ab4f682403515609557f328ab2f1d34d237
pathbox <guijihe@163.com>
2019-10-07 16:27:36 +0800

    控制如何运行

--- rust-by-example/adder/src/lib.rs
+++ rust-by-example/adder/src/lib.rs
@@ -35,6 +35,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     #[should_panic(expected = "Guess value must be less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
