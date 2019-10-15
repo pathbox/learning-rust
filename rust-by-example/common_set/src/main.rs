63d91b4562f65aeb5ee803a6a3e707c34b916b54
pathbox <guijihe@163.com>
2019-10-15 08:59:17 +0800

    Struct Counter 实现 Iterator next()

--- rust-by-example/common_set/src/main.rs
+++ rust-by-example/common_set/src/main.rs
@@ -132,3 +132,38 @@ fn filters_by_size() {
         ]
     );
 }
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        COunter {count: 0 }
+    }
+}
+
+impl Iterator for Counter { // Counter impl Iterator
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None 
+        }
+    }
+}
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+}
