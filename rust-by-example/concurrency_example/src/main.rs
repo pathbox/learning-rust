a8ba2c725e5b98b6758e232c764bd05fc1e7e437
pathbox <guijihe@163.com>
2019-10-20 21:20:31 +0800

    用join 等待所有线程结束

--- rust-by-example/concurrency_example/src/main.rs
+++ rust-by-example/concurrency_example/src/main.rs
@@ -2,7 +2,22 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    // 1. 需要调用 thread::spawn 函数并传递一个闭包
+    // thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!("hi number {} from the spawned thread!", i);
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+
+    // for i in 1..5 {
+    //     println!("hi number {} from the main thread!", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+
+    // 2. 用join 等待所有线程结束
+
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
@@ -13,6 +28,7 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
 }
 
-// 需要调用 thread::spawn 函数并传递一个闭包
