1253e438589588554b240ad2bce45ed41852e943
pathbox <guijihe@163.com>
2019-10-20 21:41:39 +0800

    move 关键字强制获取它使用的值的所有权

--- rust-by-example/concurrency_example/src/main.rs
+++ rust-by-example/concurrency_example/src/main.rs
@@ -30,5 +30,31 @@ fn main() {
     }
 
     handle.join().unwrap();
+
+    let handle1 = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    handle1.join().unwrap(); // 主线程等待上面的全部完成 才会继续执行
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // 3. move 我们在一个线程中使用另一个线程的数据 move 关键字强制闭包获取其使用的环境值的所有权
+
+    let v = vec![1, 2, 3];
+
+    let handle2 = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    // drop(v);  // move了 所以不能在主线程drop v
+
+    handle2.join().unwrap();
 }
 
