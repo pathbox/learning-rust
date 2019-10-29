6b85f4c77c13130ba94d6c0aa337ea94a5441e32
pathbox <guijihe@163.com>
2019-10-29 08:35:31 +0800

    OO simple example

---
+++ rust-by-example/oo_example/src/main.rs
@@ -0,0 +1,91 @@
+fn main() {
+     
+}
+
+pub struct AveragedCollection {
+    list: Vec<i32>,
+    average: f64,
+}
+
+impl AveragedCollection {
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            },
+            None => None,
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self){
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+}
+
+pub trait Draw {
+    fn draw(&self)
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32, 
+    pub label: String, 
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // 实际绘制按钮的代码
+    }
+}
+
+use gui::Draw; 
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
+use gui::{Screen, Button};
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10, 
+                options: vec![
+                    String::from("Yes"), 
+                    String::from("Maybe"),
+                    String::from("No")
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
+ 
