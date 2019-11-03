039c36d07cae28e344ce06a4e51f30f37f4248e5
pathbox <guijihe@163.com>
2019-11-03 14:19:38 +0800

    simple tcp server: some change

--- rust-by-example/hello_server/src/main.rs
+++ rust-by-example/hello_server/src/main.rs
@@ -22,11 +22,38 @@ fn handle_connection(mut stream: TcpStream) {
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
+    let get = b"GET / HTTP/1.1\r\n";
+
     let contents = fs::read_to_string("hello.html").unwrap(); // hello.html在项目根目录开始找
 
     // let response = "HTTP/1.1 200 OK\r\n\r\n"; // 最简单的HTTP 文本 response Status Code
 
     let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+
+    //  if buffer.starts_with(get) {
+    //     let contents = fs::read_to_string("hello.html").unwrap();
+
+    //     let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    //     stream.write(response.as_bytes()).unwrap();
+    //     stream.flush().unwrap();
+    // } else {
+    //     // 其他请求
+    // }
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
