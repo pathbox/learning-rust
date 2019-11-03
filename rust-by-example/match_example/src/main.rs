e2d94d306bc7fc25dd2b6fa86dc7c330c09f4711
pathbox <guijihe@163.com>
2019-11-03 10:52:31 +0800

    match example 2

--- rust-by-example/match_example/src/main.rs
+++ rust-by-example/match_example/src/main.rs
@@ -10,6 +10,17 @@ enum Message {
     ChangeColor(Color),
 }
 
+struct Point {
+    x: i32, 
+    y: i32,
+}
+
+struct Point1 {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
 fn main() {
     let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
 
@@ -31,5 +42,43 @@ fn main() {
             )
         }
          _ => ()
+    }
+
+    let points = vec![
+        Point { x: 0, y: 0 },
+        Point { x: 1, y: 5 },
+        Point { x:10, y: -3},
+    ];
+
+    let sum_of_squares: i32 = points.iter().map(|&Point {x, y }| x * x + y * y).sum();
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {}, {}, {}", first, third, fifth)
+        },
+    }
+
+    
+
+    let origin = Point1 { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Point1 { x, .. } => println!("x is {}", x),
     }
 }
