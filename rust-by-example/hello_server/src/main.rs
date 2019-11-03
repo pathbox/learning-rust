05e4a15548ba2741fe2925741f88fd93c1b81b77
pathbox <guijihe@163.com>
2019-11-03 14:11:49 +0800

    simple tcp server: Status Code response

--- rust-by-example/hello_server/src/main.rs
+++ rust-by-example/hello_server/src/main.rs
@@ -15,11 +15,16 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
+    let mut buffer = [0; 512]; // 0 开始  512 字节长度
 
     stream.read(&mut buffer).unwrap(); // 数据读取到buffer
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n"; // 最简单的HTTP 文本 response Status Code
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
 
