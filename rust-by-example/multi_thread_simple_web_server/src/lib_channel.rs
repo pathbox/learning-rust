e48eb2a5c488ee2c90ad97fb164af6bd943f9a18
pathbox <guijihe@163.com>
2019-11-05 09:20:06 +0800

    multiple thread pool web server:good

--- rust-by-example/multi_thread_simple_web_server/src/lib_channel.rs
+++ rust-by-example/multi_thread_simple_web_server/src/lib_channel.rs
@@ -1,45 +1,86 @@
 use std::thread;
-
-use std::sync::mpsc
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+// --snip--
 
 pub struct ThreadPool {
-  workers: Vec<Worker>,
-  sender: mpsc::Sender<Job>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
-
 struct Job;
-
 impl ThreadPool {
-  pub fn new(size: usize) -> ThreadPool {
-    assert!(size > 0);
+    // --snip--
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
 
-    let (sender, receiver) = mpsc::channel();
+        let (sender, receiver) = mpsc::channel();
 
-    let mut workers = Vec::with_capacity(size);
+        let receiver = Arc::new(Mutex::new(receiver));
 
-    for id in 0..size {
-      workers.push(Worker::new(id));
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool {
+            workers,
+            sender,
+        }
+    }
+  
+    pub fn execute<F>(&self, f: F)
+      where
+          F: FnOnce() + Send + 'static 
+    {
+      let job = Box::new(f);
+
+      self.sender.send(job).unwrap();
     }
 
-    ThreadPool {
-      workers,
-      sender,
-    }
-  }
+    // --snip--
 }
 
 struct Worker {
-  id: usize,
-  thread: thread::JoinHandle<()>,
+    id: usize,
+    thread: thread::JoinHandle<()>,
 }
 
-impl Worker {
-  fn new(id: usize) -> Wokrer {
-    let thread = thread::spawn(|| {});
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
 
-    Worker {
-      id, 
-      thread,
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
     }
-  }
+}
+
+type Job = Box<FnBox + Send + 'static>;
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        // --snip--
+        let thread = thread::spawn(move || {
+          // loop {
+          //   let job = receiver.lock().unwrap().recv().unwrap();
+
+          //   println!("Worker {} got a job; executing.", id);
+            
+          //   job.call_box();
+          // }
+           while let Ok(job) = receiver.lock().unwrap().recv() {
+                println!("Worker {} got a job; executing.", id);
+
+                job.call_box();
+            }
+        });
+
+
+        Worker {
+            id,
+            thread,
+        }
+    }
 }
