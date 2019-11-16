0af25abc9b8e323643f32ac4df1415150da56047
pathbox <guijihe@163.com>
2019-11-16 16:08:10 +0800

    Formatting

--- rust-by-example/print/src/main.rs
+++ rust-by-example/print/src/main.rs
@@ -1,5 +1,5 @@
-use std::fmt;
 
+use std::fmt::{self, Formatter, Display};
 #[derive(Debug)]
 struct MinMax(i64, i64);
 
@@ -21,6 +21,31 @@ impl fmt::Display for Point2D {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "x===: {}, y===: {}", self.x, self.y)
     }
+}
+
+struct City {
+    name: &'static str,
+    // Latitude
+    lat: f32,
+    // Longitude
+    lon: f32,
+}
+
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
 }
 
 #[derive(Debug)]
@@ -75,4 +100,21 @@ fn main() {
     println!("Compare points:");
     println!("Display: {}", point); // 如果没有impl fmt::Display for Point2D 这里是会报错的，{} 是默认的formatter 不能格式化打印struct
     println!("Debug: {:?}", point);
+
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{:?}", *color);
+    }
 }
