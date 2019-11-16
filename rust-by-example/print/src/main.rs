03454808273be77a10f5da3d3634e833ba9d5276
pathbox <guijihe@163.com>
2019-11-16 14:45:48 +0800

    impl fmt::Display for struct

--- rust-by-example/print/src/main.rs
+++ rust-by-example/print/src/main.rs
@@ -1,3 +1,28 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+// Implement `Display` for `MinMax`
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // use self.number to refer to each positional data point
+        write!(f, "({},{})", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct Point2D{
+    x: f64,
+    y: f64,
+}
+
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x===: {}, y===: {}", self.x, self.y)
+    }
+}
+
 #[derive(Debug)]
 struct Deep(Structure);
 
@@ -33,4 +58,21 @@ fn main() {
     let peter = Person {name, age};
 
     println!("{:#?}", peter);
+
+    let minmax = MinMax(0, 14);
+
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!("The big range is {big} and the small is {small}", small=small_range, big = big_range);
+
+    let point = Point2D {x: 3.3, y: 7.2};
+
+    println!("Compare points:");
+    println!("Display: {}", point); // 如果没有impl fmt::Display for Point2D 这里是会报错的，{} 是默认的formatter 不能格式化打印struct
+    println!("Debug: {:?}", point);
 }
