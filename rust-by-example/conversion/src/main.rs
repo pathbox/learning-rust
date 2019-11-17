07391de443fb83ab59a6a5a4188292a34b6024c3
pathbox <guijihe@163.com>
2019-11-17 10:10:11 +0800

    Conversion try_from try_into example

--- rust-by-example/conversion/src/main.rs
+++ rust-by-example/conversion/src/main.rs
@@ -1,8 +1,25 @@
 use std::convert::From; 
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
 #[derive(Debug)]
 struct Number {
     value: i32,
+}
+
+#[derive(Debug, PartialEq)]
+struct EvenNumber(i32);
+
+impl TryFrom<i32> for EvenNumber {
+    type Error = ();
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        if value % 2 == 0 {
+            Ok(EvenNumber(value))
+        } else {
+            Err(())
+        }
+    }
 }
 
 // impl From method
@@ -20,4 +37,10 @@ fn main() {
     let int = 5;
     let num: Number = int.into(); // The same as let num = Number::from(5);
     println!("My number is {:?}", num);
+
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
 }
