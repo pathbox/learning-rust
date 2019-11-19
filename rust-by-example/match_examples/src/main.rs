200072a56be06326aa46622c31a4b4a5e1ca212d
pathbox <guijihe@163.com>
2019-11-19 09:35:12 +0800

    match example 1

---
+++ rust-by-example/match_examples/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    println!("match example");
+
+    let number = 13; 
+
+    println!("Tell me about {}", number);
+    match number {
+        // Match a single value
+        1 => println!("One!"),
+        // Match several values
+        2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
+        // Match an inclusive range
+        13...19 => println!("A teen"),
+        // Handle the rest of cases
+        _ => println!("Ain't special"),
+    }
+
+    let boolean = true;
+    let binary = match boolean {
+        false => 0,
+        true => 1,
+    }
+    println!("{} -> {}", boolean, binary);
+}
