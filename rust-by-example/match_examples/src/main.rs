90cc0ff6440b5778c02de8e1f8ec8cc7a14a7761
pathbox <guijihe@163.com>
2019-11-24 11:54:34 +0800

    function and closure

--- rust-by-example/match_examples/src/main.rs
+++ rust-by-example/match_examples/src/main.rs
@@ -9,6 +9,14 @@ enum Color {
     CMYK(u32,u32,u32,u32),
 }
 
+// Our example enum
+enum Foo {
+    Bar,
+    Baz,
+    Qux(u32)
+}
+
+
 fn main() {
     println!("match example");
 
@@ -124,4 +132,32 @@ fn main() {
         //Foo { y } => println!("y = {}", y);
 
     }
+
+     // Create example variables
+    let a = Foo::Bar;
+    let b = Foo::Baz;
+    let c = Foo::Qux(100);
+    
+    // Variable a matches Foo::Bar
+    if let Foo::Bar = a {
+        println!("a is foobar");
+    }
+    
+    // Variable b does not match Foo::Bar
+    // So this will print nothing
+    if let Foo::Bar = b {
+        println!("b is foobar");
+    }
+    
+    // Variable c matches Foo::Qux which has a value
+    // Similar to Some() in the previous example
+    if let Foo::Qux(value) = c {
+        println!("c is {}", value);
+    }
+
+    // Binding also works with `if let`
+    if let Foo::Qux(value @ 100) = c {
+        println!("c is one hundred");
+    }
+
 }
