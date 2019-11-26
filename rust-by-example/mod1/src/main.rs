4236573cf737dd6dedba38bcab3052d82918ca20
pathbox <guijihe@163.com>
2019-11-26 08:56:46 +0800

    struct visibility

--- rust-by-example/mod1/src/main.rs
+++ rust-by-example/mod1/src/main.rs
@@ -79,6 +79,25 @@ fn function() {
     println!("called `function()`");
 }
 
+mod my {
+    pub struct OpenBox<T> {
+        pub contents: T,
+    }
+
+    #[allow(dead_code)]
+    pub struct ClosedBox<T> {
+        contents: T,
+    }
+
+    impl<T> ClosedBox<T> {
+        pub fn new(contents: T) -> ClosedBox<T> {
+            ClosedBox {
+                contents: contents,
+            }
+        }
+    }
+}
+
 fn main() {
     function();
 
@@ -113,5 +132,17 @@ fn main() {
     // Error! `private_nested` is a private module
     //my_mod::private_nested::restricted_function();
     // TODO ^ Try uncommenting this line
+
+    let open_box = my::OpenBox { contents: "public information" };
+    // and their fields can be normally accessed.
+    println!("The open box contains: {}", open_box.contents);
+
+    // Public structs with private fields cannot be constructed using field names.
+    // Error! `ClosedBox` has private fields
+    //let closed_box = my::ClosedBox { contents: "classified information" };
+    // TODO ^ Try uncommenting this line
+
+    // However, structs with private fields can be created using
+    // public constructors
     
 }
