84710cb1eb9d931410ff11ed834b78cc49316c03
pathbox <guijihe@163.com>
2019-12-01 17:17:28 +0800

    vi

--- rust-by-example/generic/src/main.rs
+++ rust-by-example/generic/src/main.rs
@@ -1,3 +1,16 @@
+use std::fmt::Debug;
+
+trait PrintInOption {
+    fn print_in_option(self);
+}
+
+impl<T> PrintInOption for T where
+    Option<T>: Debug {
+        fn print_in_option(self) {
+            println!("{:?}", Some(self));
+        }
+    }
+
 struct Val {
     val: f64,
 }
@@ -18,8 +31,72 @@ impl<T> GenVal<T> {
     }
 }
 
+struct Years(i64);
+
+struct Days(i64);
+
+impl Years {
+    pub fn to_days(&self) -> Days {
+        Days(self.0 * 365)
+    }
+}
+
+impl Days {
+    pub fn to_years(&self) -> Years {
+        Years(self.0 / 365)
+    }
+}
+
+fn old_enough(age: &Years) -> bool {
+    age.0 >= 18
+}
+
+struct Container(i32, i32);
+
+trait Contains<A,B> {
+    fn contains(&self, _: &A, _: &B) -> bool;
+    fn first(&self) -> i32;
+    fn last(&self) -> i32;
+}
+
+impl Contains<i32, i32> for Container {
+    fn contains(&self,number_1: &i32, number_2: &i32) -> bool {
+        (&self.0 == number_1) && (&self.1 == number_2)
+    }
+
+    fn first(&self) -> i32 { self.0 }
+
+    fn last(&self) -> i32 { self.1 }
+}
+
+// `C` contains `A` and `B`. In light of that, having to express `A` and
+// `B` again is a nuisance.
+fn difference<A, B, C>(container: &C) -> i32 where
+    C: Contains<A, B> {
+    container.last() - container.first()
+}
+
 fn main() {
     let x = Val { val: 3.0 };
     let y = GenVal { gen_val: 3i32 };
     println!("{}, {}", x.value(), y.value());
+
+    let age = Years(5);
+    let age_days = age.to_days();
+    println!("Old enough {}", old_enough(&age));
+    println!("Old enough {}", old_enough(&age_days.to_years()));
+    // println!("Old enough {}", old_enough(&age_days));
+
+    let number_1 = 3;
+    let number_2 = 10;
+
+    let container = Container(number_1, number_2);
+
+    println!("Does container contain {} and {}: {}",
+        &number_1, &number_2,
+        container.contains(&number_1, &number_2));
+    println!("First number: {}", container.first());
+    println!("Last number: {}", container.last());
+
+    println!("The difference is: {}", difference(&container));
 }
