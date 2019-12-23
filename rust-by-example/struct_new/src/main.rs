4e1e762b37441dd83c8391e8537c238379ba97f1
pathbox <guijihe@163.com>
2019-12-23 09:30:11 +0800

    vi

--- rust-by-example/struct_new/src/main.rs
+++ rust-by-example/struct_new/src/main.rs
@@ -30,6 +30,29 @@ impl CircleBuilder {
     }
 }
 
+trait HasArea {
+    fn area(&self) -> f64; 
+    fn is_larger(&self, &Self) -> bool;
+}
+
+impl HasArea for Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+
+    fn is_larger(&self, other: &self) -> bool {
+        self.area() > other.area()
+    }
+}
+
+use std::fmt::Debug;
+
+fn bar<T, K>(x: T, y: K) where T: Clone, K: Clone + Debug {
+    x.clone();
+    y.clone();
+    println!("{:?}",y);
+}
+
 fn main() {
     let c = CircleBuilder::new()
                 .x(1.0)
@@ -39,5 +62,29 @@ fn main() {
     
     println!("area: {}", c.area());
     println!("x: {}", c.x);
-    println!("y: {}", c.y);          
+    println!("y: {}", c.y);       
+    bar("Hello", "world");   
 }
+
+trait ConvertTo<Output> {
+    fn convert(&self) -> Output;
+}
+impl ConvertTo<i64> for i32 {
+    fn convert(&self) -> i64 { *self as i64 }
+}
+// Can be called with T == i32.
+fn normal<T: ConvertTo<i64>>(x: &T) -> i64 {
+    x.convert()
+}
+// Can be called with T == i64.
+fn inverse<T>(x: i32) -> T
+    // This is using ConvertTo as if it were "ConvertTo<i64>
+    ".
+    where i32: ConvertTo<T> {
+        x.convert()
+}
+
+//这突显出了 where 从句的额外的功能：它允许限制的左侧可以是任意类型（在这
+// 里是 i32 ），而不仅仅是一个类型参数（比如 T ）。在这个例子中， i32 必须
+// 实现 ConvertTo<T> 。不同于定义 i32 是什么（因为这是很明显的），这里
+// 的 where 从句限制了 T
