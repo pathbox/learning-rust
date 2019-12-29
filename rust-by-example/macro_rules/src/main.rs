288ff983cbc54aeaeff248efc1fb3fa547e72c50
pathbox <guijihe@163.com>
2019-12-29 10:45:26 +0800

    递归宏: write_html

--- rust-by-example/macro_rules/src/main.rs
+++ rust-by-example/macro_rules/src/main.rs
@@ -43,6 +43,20 @@ macro_rules! o_O {
     }
 }
 
+#[allow(unused_must_use)]
+macro_rules! write_html {
+    ($w:expr, ) => (());
+
+    ($w:expr, $e:tt) => (write!($w, "{}", $e));
+
+    ($w:expr, $tag:ident [ $($inner:tt)* ] $($rest:tt)*) => {{
+        write!($w, "<{}>", stringify!($tag));
+        write_html!($w, $($inner)*);
+        write!($w, "</{}>", stringify!($tag));
+        write_html!($w, $($rest)*);
+    }};
+}
+
 fn main() {
     foo();
     bar();
@@ -66,4 +80,16 @@ fn main() {
     let a: &[i32]
         = o_O!(10; [1,2,3]; 20; [4,5,6]);
     println!("o_O: {:?}", a);
+
+    use std::fmt::Write;
+    let mut out = String::new();
+    write_html!(&mut out,
+    html[
+    head[title["Macros guide"]]
+    body[h1["Macros are the best!"]]
+    ]);
+    assert_eq!(out,
+    "<html><head><title>Macros guide</title></head>\
+    <body><h1>Macros are the best!</h1></body></html>");
+    println!("out: {}",out);
 }
