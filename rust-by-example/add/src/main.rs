c5e1a0be9663f3954fb5f227f8e297b6d59a3b5c
pathbox <guijihe@163.com>
2019-12-29 09:47:14 +0800

    在泛型结构体中使用运算符 trait

--- rust-by-example/add/src/main.rs
+++ rust-by-example/add/src/main.rs
@@ -1,6 +1,26 @@
 use std::ops::Add;
-#[derive(Debug)]
+use std::ops::Mul;
 
+trait HasArea<T> {
+    fn area<&self> -> T;
+}
+
+struct Square<T> {
+    x: T,
+    y: T,
+    side: T,
+}
+
+impl<T> HasArea<T> for Square<T>
+        where T: Mul<Output=T> + Copy {
+    fn area(&self) -> T {
+        self.side * self.side
+    }
+}
+
+
+
+#[derive(Debug)]
 struct Point {
     x: i32,
     y: i32,
@@ -17,4 +37,11 @@ fn main() {
     let p2 = Point { x: 2, y: 3 };
     let p3 = p1 + p2;
     println!("{:?}", p3);
+
+    let s = Square {
+        x: 0.1f64,
+        y: 0.2f64,
+        side: 12.0f64,
+    };
+    println!("Area of s: {}", s.area());
 }
