5f2f108b61ea68b1692e7a1d888c0d00c16369f3
pathbox <cary.luo@ucloud.cn>
2020-01-28 21:20:05 +0800

    openssh-keys example

---
+++ openssh-keys/example/inspect_rsa.rs
@@ -0,0 +1,19 @@
+extern crate dirs;
+extern crate openssh_keys;
+
+use std::{fs, io, path};
+use std::io::BufRead;
+
+fn main() {
+  let home = dirs::home_dir().unwrap_or_else(|| path::PathBuf::from("/home/pathbox/"));
+  let pub_path = home.join(".ssh").join("id_rsa.pub");
+  println!("Inspecting '{}':", pub_path.to_string_lossy());
+  let file = fs::File::open(&pub_path).expect("unable to open RSA pubkey");
+  let reader = io::BufReader::new(file);
+
+  for (i, line) in reader.lines().enumerate() {
+        let line = line.expect(&format!("unable to read key at line {}", i + 1));
+        let pubkey = openssh_keys::PublicKey::parse(&line).expect("unable to parse RSA pubkey");
+        println!(" * Pubkey #{} -> {}", i + 1, pubkey.to_fingerprint_string());
+    }
+}
